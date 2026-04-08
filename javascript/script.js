a4f6cf6cdc54e057ca081c5b752455f1de51e300
ABadMinecraftPlayer <Jasperadirakelarsson@gmail.com>
2026-04-08 10:45:09 +0200

    Completely finished the footer. Init javascript, added js to info, which changes the main image.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,8 @@
-function sent() {
-    print("true");
+
+// Swaps the 
+function SwapInfo(x) {
+    var swap = document.getElementById(`swapinfo_${x}`);
+    var toswap = document.getElementById("card-1_display_img");
+
+    toswap.style.backgroundImage = `url(${swap.src})`;
 }
