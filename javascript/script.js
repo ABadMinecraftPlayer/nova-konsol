b069eabef10513f6d789e65504140a83d793bd4c
ABadMinecraftPlayer <Jasperadirakelarsson@gmail.com>
2026-04-09 11:04:33 +0200

    Init side-scroll_product_info

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,14 @@
 
-// Swaps the 
+// Swaps the card-1_display_img background image to the one of the clicked image in the side-scroll_product_info
 function SwapInfo(x) {
     var swap = document.getElementById(`swapinfo_${x}`);
     var toswap = document.getElementById("card-1_display_img");
 
     toswap.style.backgroundImage = `url(${swap.src})`;
+}
+
+function AutoManualSwap(x) {
+    setTimeout(() => {
+        
+    }, 5000);
 }
